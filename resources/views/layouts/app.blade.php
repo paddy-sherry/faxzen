54ead0ae3e1ba32afb00d01be5ac3a97a791c222
paddy sherry <hello@paddysherry.com>
2025-06-16 01:13:05 +0200

    favixcon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'FaxZen - Send Faxes Online')</title>
+    <link rel="icon" type="image/gif" href="https://imagedelivery.net/k0P4EcPiouU_XzyGSmgmUw/fb1d0ae8-0136-4e6a-bc03-fe4e8d30f300/public">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
